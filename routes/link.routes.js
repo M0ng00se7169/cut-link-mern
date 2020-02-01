e820291e6e4c05d83af4f3b56b81be8dd0628cc3
M0ng00se7169 <NickKomarowski@gmail.com>
2020-02-01 14:36:32 +0100

    Fixing proxy error 3

--- routes/link.routes.js
+++ routes/link.routes.js
@@ -18,7 +18,7 @@ router.post('/generate', auth, async (req, res) => {
       return res.json({ link: existing })
     }
 
-    const to = baseUrl + 't/' + code
+    const to = baseUrl + '/t/' + code
     
     const link = new Link({
       code,
